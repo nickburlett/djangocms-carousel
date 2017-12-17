f4e9bf991042dc035cf1a717c14779f582e22ff8
Nicholas Burlett <nickburlett@mac.com>
2017-12-17 10:57:11 -0800

    Update tests for py3.6, full test coverage
    1. Add test support for python 3.6
    2. Add basic tests that get full test coverage

--- tests/test_cmsplugin_carousel.py
+++ tests/test_cmsplugin_carousel.py
@@ -1,6 +1,50 @@
+from cms.api import add_plugin
+from cms.models import Placeholder
+from cms.plugin_rendering import ContentRenderer
+from django.test import TestCase
+from django.test.client import RequestFactory
 
-import cmsplugin_carousel
+from cmsplugin_carousel.cms_plugins import CMSCarouselPlugin
+from cmsplugin_carousel.models import CarouselPlugin
 
 
-def test_main():
-    assert cmsplugin_carousel  # use your library here
+class CMSCarouselPluginTest(TestCase):
+    def test_plugin_context(self):
+        placeholder = Placeholder.objects.create(slot='test')
+        model_instance = add_plugin(
+            placeholder,
+            CMSCarouselPlugin,
+            'en',
+        )
+        plugin_instance = model_instance.get_plugin_class_instance()
+        context = plugin_instance.render({}, model_instance, None)
+        self.assertIn('rotate_interval', context)
+        self.assertEqual(context['rotate_interval'], 5000)
+
+    def test_plugin_html(self):
+        placeholder = Placeholder.objects.create(slot='test')
+        model_instance = add_plugin(
+            placeholder,
+            CMSCarouselPlugin,
+            'en',
+        )
+        renderer = ContentRenderer(request=RequestFactory())
+        html = renderer.render_plugin(model_instance, {})
+
+        self.assertIn('id="carousel_1"', html)
+        self.assertIn('data-interval="5000"', html)
+        self.assertIn('<ol class="carousel-indicators">', html)
+        self.assertIn('<div class="carousel-inner">', html)
+
+    def test_plugin_stringified(self):
+        placeholder = Placeholder.objects.create(slot='test')
+        model_instance = add_plugin(
+            placeholder,
+            CMSCarouselPlugin,
+            'en',
+        )
+        self.assertEqual(str(model_instance), str(model_instance.pk))
+
+        model_instance.title = "Test Plugin"
+
+        self.assertEqual(str(model_instance), "Test Plugin")
